e0bb8542e7f60a7eea1f765b021eb2b982f712b4
Eglia Koncurat <egliakoncurat@Eglias-MacBook-Pro.local>
2025-04-17 16:58:52 -0600

    terraform

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project" {
+    type= string
+    description = "ID Google project"
+    default = "gothic-jigsaw-457115-v3"
+}
+
+variable "region" {
+    type= string
+    description = "Region Google project"
+    default = "us-central1"
+}

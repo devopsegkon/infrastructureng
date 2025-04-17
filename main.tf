3eb2f016e76ccbc1fe15a67afee0026ad3766ec5
Eglia Koncurat <egliakoncurat@Eglias-MacBook-Pro.local>
2025-04-17 17:34:59 -0600

    terraform

--- main.tf
+++ main.tf
@@ -10,53 +10,53 @@ provider "google" {
   region = var.region
 }
 
-resource "google_cloud_run_service" "default" {
-  name     = "cloudrun-srv"
-  location = "us-central1"
+#resource "google_cloud_run_service" "default" {
+#  name     = "cloudrun-srv"
+#  location = "us-central1"
 
-  template {
-    spec {
-      containers {
-        image = var.image
-        startup_probe {
-          initial_delay_seconds = 10
-          timeout_seconds = 5
-          period_seconds = 3
-          failure_threshold = 1
-          tcp_socket {
-            port = 8080
-          }
-        }
-        liveness_probe {
-          http_get {
-            path = "/"
-          }
-        }
-      }
-    }
-  }
+#  template {
+#    spec {
+#      containers {
+#        image = var.image
+#        startup_probe {
+#          initial_delay_seconds = 10
+#          timeout_seconds = 5
+#          period_seconds = 3
+#          failure_threshold = 1
+#          tcp_socket {
+#            port = 8080
+#          }
+#        }
+#        liveness_probe {
+#          http_get {
+#            path = "/"
+#          }
+#        }
+#      }
+#    }
+#  }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+ # traffic {
+ #   percent         = 100
+ #   latest_revision = true
+ # }
 
-  lifecycle {
-    ignore_changes = [
-      metadata.0.annotations,
-    ]
-  }
-}
+ # lifecycle {
+ ##   ignore_changes = [
+ #     metadata.0.annotations,
+#    ]
+#  }
+#}
 
-resource "google_cloud_run_service_iam_policy" "public_access" {
-  service = google_cloud_run_service.default.name
-  location = google_cloud_run_service.default.location
-  policy_data = data.google_iam_policy.public_iam_policy.policy_data
-}
+#resource "google_cloud_run_service_iam_policy" "public_access" {
+#  service = google_cloud_run_service.default.name
+#  location = google_cloud_run_service.default.location
+#  policy_data = data.google_iam_policy.public_iam_policy.policy_data
+#}
 
-data "google_iam_policy" "public_iam_policy" {
-  binding {
-    role = "roles/run.invoker"
-    members = ["allUsers"]
-  }
-}
+#data "google_iam_policy" "public_iam_policy" {
+#  binding {
+#    role = "roles/run.invoker"
+#    members = ["allUsers"]
+#  }
+#}
